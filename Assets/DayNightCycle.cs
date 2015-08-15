ec031a8ee047fef8fc8c8c93c15e8c8514963247
Dries Goossens <driesgoossens93@gmail.com>
2015-08-15 14:30:29 +0200

    Day night cycle
    added basic day and night cycle

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+	public float timer=0.0f;
+	public bool day=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!day) {
+			//licht worden
+			timer += Time.deltaTime;
+			day = (timer >= 10.0f);
+		} else {
+			//donker worden
+			timer-=Time.deltaTime;
+			day = !(timer <= 0.0f);
+		}
+		GetComponent<Light>().intensity=timer*0.75f/10.0f;
+
+	}
+}
